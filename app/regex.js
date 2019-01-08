7caa4d08988b419fe2b546673dc3922fe5a63f9b
Drew Terry <drew.terry@skookum.com>
2019-01-08 12:10:06 -0500

    npm install and regexes

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,27 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    return /[0-9]/.test(str)
   },
 
   containsRepeatingLetter: function(str) {
-
+    return /([a-zA-z])\1+/.test(str)
   },
 
   endsWithVowel: function(str) {
-
+    return /[aeiou]$/i.test(str)
   },
 
   captureThreeNumbers: function(str) {
-
+    matches = /[0-9]{3}/.exec(str)
+    return matches ? matches[0] : false
   },
 
   matchesPattern: function(str) {
-
+    return /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(str)
   },
 
   isUSD: function(str) {
-
+    return /^\$[0-9]{1,3},?([0-9]{3},?)*(\.[0-9]{2}){0,1}$/.test(str)
   }
 };
