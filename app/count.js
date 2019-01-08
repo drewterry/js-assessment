7c4d2c42e8502ad8409cf3280283e321a88a03ae
Drew Terry <drew.terry@skookum.com>
2019-01-08 15:27:37 -0500

    count, best practices

--- app/count.js
+++ app/count.js
@@ -2,6 +2,18 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    var ref
 
+    (function output(count) {
+      console.log(count)
+      
+      if (count !== end) {
+        ref = setTimeout(output, 100, ++count)
+      }
+    })(start)
+
+    return {
+      cancel: () => clearTimeout(ref)
+    }
   }
 };
